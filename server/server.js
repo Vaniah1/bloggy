6169e93f1692890be249437176b9965883f0745c
Vaniah1 <ndunguvaniah@gmail.com>
2024-05-29 16:48:57 +0300

    auth

--- server/server.js
+++ server/server.js
@@ -1,19 +1,21 @@
-import "dotenv/config"
-import express from "express"
-import mongoose from "mongoose"
-import bcrypt from "bcrypt"
+import "dotenv/config";
+import express from "express";
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
+//schemas here
+import User from "./Schema/User.js";
 
-const server =express()
-let PORT = 3000
+const server = express();
+let PORT = 3000;
 let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
 let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
 
-mongoose.connect(process.env.DB_LOCATION,{
-    autoIndex:true
-})
+mongoose.connect(process.env.DB_LOCATION, {
+  autoIndex: true,
+});
 
-server.use(express.json())
+server.use(express.json());
 
 server.post("/signup", (req, res) => {
   const { fullname, email, password } = req.body;
@@ -32,21 +34,36 @@ server.post("/signup", (req, res) => {
   }
 
   if (!passwordRegex.test(password)) {
-    return res
-      .status(403)
-      .json({
-        error:
-          "password should be 6 to 20 characters long with a numeric, 1 lowercase and 1 uppercase letters",
-      });
+    return res.status(403).json({
+      error:
+        "password should be 6 to 20 characters long with a numeric, 1 lowercase and 1 uppercase letters",
+    });
   }
 
   bcrypt.hash(password, 10, (err, hashed_password) => {
-    console.log(hashed_password);
+    let username = email.split("@")[0];
+
+    let user = new User({
+      personal_info: { fullname, email, password: hashed_password, username },
+    });
+
+    user
+      .save()
+      .then((u) => {
+        return res.status(200).json({ user: u });
+      })
+      .catch((err) => {
+        if (err.code == 11000) {
+          return res
+            .status(500)
+            .json({ error: "email address already exists" });
+        }
+        return res.status(500).json({ error: err.message });
+      });
   });
-  return res.status(200).json({ status: "Okey" });
 });
 server.listen(PORT, () => {
-    console.log('------------------------------------');
-    console.log("Listening on port " + PORT);
-    console.log("-------------------------------------");
-})
+  console.log("------------------------------------");
+  console.log("Listening on port " + PORT);
+  console.log("-------------------------------------");
+});
