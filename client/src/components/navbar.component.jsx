a69c8346b5c6751f2fa318576615dc663d3da0ae
Vaniah1 <ndunguvaniah@gmail.com>
2024-05-29 08:11:06 +0300

    navbar

--- client/src/components/navbar.component.jsx
+++ client/src/components/navbar.component.jsx
@@ -8,12 +8,13 @@ const Navbar = () => {
       <Link to="/" className="flex-none w-10">
         <img src={logo} alt="" className="w-full" />
       </Link>
-      <div className="absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey">
+      <div className="py-4 px-[5vw] absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey">
         <input
           type="text"
           placeholder="Search"
-          className="w-full md:w-auto bg-grey p-4 pl-6 pr-[12%] md:pr-6 rounded-full  "
+          className="w-full md:w-auto bg-grey p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-grey "
         />
+        <i class="fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey"></i>
       </div>
     </nav>
   );
