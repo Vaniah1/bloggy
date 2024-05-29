5d552a8cbc07cb86e623cc3a0293ac6ffff1363b
Vaniah1 <ndunguvaniah@gmail.com>
2024-05-29 07:58:03 +0300

    navbar

--- client/src/components/navbar.component.jsx
+++ client/src/components/navbar.component.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import logo from "../imgs/logo.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <h1>Navbar</h1>;
+  return (
+    <nav className="navbar">
+      <Link to="/" className="flex-none w-10">
+        <img src={logo} alt="" className="w-full" />
+      </Link>
+      <div className="absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey">
+        <input
+          type="text"
+          placeholder="Search"
+          className="w-full md:w-auto bg-grey p-4 pl-6 pr-[12%] md:pr-6 rounded-full  "
+        />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
